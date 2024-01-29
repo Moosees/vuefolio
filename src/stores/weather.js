610c506f1b0590d86fb30598167b3a62cc10121d
Moosees <linus.almgren@gmail.com>
2024-01-29 23:23:34 +0100

    add parsing to weather data to make it easier to use

--- src/stores/weather.js
+++ src/stores/weather.js
@@ -3,22 +3,22 @@ import { ref } from 'vue';
 
 export const useWeatherStore = defineStore('weather', () => {
 	const temperature = ref(0); // °C
-	const isDay = ref(false); // 0 or 1
-	const rain = ref(0); // mm
-	const snowFall = ref(0); // cm
-	const snowDepth = ref(0); // m
+	const isDay = ref(false); // boolean
+	const rain = ref(0); // mm (last 15 minutes)
+	const snowFall = ref(0); // cm (last 15 minutes)
+	const snowDepth = ref(0); // cm (on ground)
 	const cloudCover = ref(0); // %
 	const windSpeed = ref(0); // m/s
-	const windDirectionFromEast = ref(true);
+	const windDirectionFromEast = ref(true); // boolean
 
 	const setWeatherData = (newData) => {
 		temperature.value = Math.floor(newData.current.temperature_2m);
 		isDay.value = !!newData.current.is_day;
 		rain.value = newData.current.rain;
 		snowFall.value = newData.current.snowfall;
-		snowDepth.value = newData.hourly.snow_depth[newData.hourly.snow_depth.length - 1];
+		snowDepth.value = newData.hourly.snow_depth[newData.hourly.snow_depth.length - 1] * 100;
 		cloudCover.value = newData.current.cloud_cover;
-		windSpeed.value = newData.current.wind_speed_10m;
+		windSpeed.value = Math.floor(newData.current.wind_speed_10m);
 		windDirectionFromEast.value = newData.current.wind_direction_10m < 180;
 	};
 
