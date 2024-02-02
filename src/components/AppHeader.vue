1ff825bc8f53efcfcadd8fc76c4ff3b8207d7136
Moosees <linus.almgren@gmail.com>
2024-02-02 18:51:32 +0100

    make link styles global

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -29,19 +29,9 @@ nav {
 	justify-content: space-between;
 }
 
-nav a {
+a {
 	color: #fefefe;
 	font-size: 1.2em;
-	text-decoration: underline currentColor 3px;
-}
-
-nav a:not(.router-link-exact-active):hover {
-	text-decoration-color:forestgreen;
-}
-
-nav a.router-link-exact-active {
-	cursor: default;
-	opacity: 0.5;
 }
 
 .group {
